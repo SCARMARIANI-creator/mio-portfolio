734835d9a13c75f294a90b0c767ff477d9ab7214
SCARMARIANI-creator <debora.mariani@veronatrento.it>
2026-07-20 10:49:13 +0200

    Add files via upload

---
+++ portfolio/immagini/disegni.js
@@ -0,0 +1,14 @@
+const mieiDisegni = [
+  {
+    titolo: "Ritratto a Matita Collab",
+    tecnica: "Penna su Carta",
+    immagine: "immagini/disegno1.jpg",
+    descrizione: "interpretazione del mio stile di un personaggio,il mio stile consiste nel rendere lo stile manga iperealistico e horror, ho usato diverse biro per realizzare il disegno."
+  },
+  {
+    titolo: "Paesaggio Digitale",
+    tecnica: "Arte Digitale",
+    immagine: "immagini/disegno2.jpg",
+    descrizione: "Realizzato in digitale."
+  }
+];*
