c6dfe5f51ca0fb7bb1d05f0fe0a050f14c6c984a
SCARMARIANI-creator <debora.mariani@veronatrento.it>
2026-07-20 11:20:04 +0200

    Update disegni.js

--- disegni.js
+++ disegni.js
@@ -2,7 +2,18 @@ const mieiDisegni = [
   {
     titolo: "Ritratto a Matita Collab",
     tecnica: "Penna su Carta",
-    immagine: "immagini/immagineprova.jpg", // <-- Senza 'portfolio/' davanti!
-    descrizione: "interpretazione del mio stile di un personaggio,il mio stile consiste nel rendere lo stile manga iperealistico e horror, ho usato diverse biro per realizzare il disegno."
+    immagine: "immagini/immagineprova.jpg",
+    concetto: "Un'interpretazione personale che unisce lo stile manga con elementi iperrealistici e sfumature horror, focalizzata sull'espressività e sul contrasto netto.",
+    dettagliTecnici: "Realizzato interamente a mano libera utilizzando diverse tipologie di penne a sfera (biro) per dosare la pressione e creare sfumature graduali di nero e grigio."
   }
+  /* 
+  Esempio per aggiungere un secondo lavoro:
+  ,{
+    titolo: "Titolo Secondo Disegno",
+    tecnica: "Acquerello e China",
+    immagine: "immagini/nome-file.jpg",
+    concetto: "Descrizione del concetto artistico...",
+    dettagliTecnici: "Dettagli sui materiali usati..."
+  }
+  */
 ];
