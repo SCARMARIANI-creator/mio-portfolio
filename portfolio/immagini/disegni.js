13343e1ab607efd2c10cc3451ee475b68f941a9b
SCARMARIANI-creator <debora.mariani@veronatrento.it>
2026-07-20 11:02:30 +0200

    Update disegni.js

--- portfolio/immagini/disegni.js
+++ portfolio/immagini/disegni.js
@@ -5,10 +5,4 @@ const mieiDisegni = [
     immagine: "immagini/immagineprova.jpg",
     descrizione: "interpretazione del mio stile di un personaggio,il mio stile consiste nel rendere lo stile manga iperealistico e horror, ho usato diverse biro per realizzare il disegno."
   },
-  {
-    titolo: "Paesaggio Digitale",
-    tecnica: "Arte Digitale",
-    immagine: "immagini/disegno2.jpg",
-    descrizione: "Realizzato in digitale."
-  }
-];*
+];
