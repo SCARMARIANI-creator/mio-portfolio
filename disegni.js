fcfc69c726bd39cd0d1022217443e843b196ddbf
SCARMARIANI-creator <debora.mariani@veronatrento.it>
2026-07-29 10:59:51 +0200

    Update disegni.js

--- disegni.js
+++ disegni.js
@@ -1,18 +1,34 @@
 const infoArtista = {
-  nome: "Art by DarkManga",
   nomeLogo: "darkmanga",
-  slogan: "Manga Artist & Illustrator | Signed by Scar",
-  biografia: "Ciao! Sono Scar, fumettista e illustratore dietro al progetto DarkManga. Benvenuti nel mio portfolio personale.",
-  instagram: "https://www.instagram.com/il_tuo_instagram",
-  email: "tuaemail@esempio.com",
-  mastodon: "",
-  linktree: ""
+  nome: "Art by Scar",
+  slogan: "Digital Artist",
+  biografia: "Benvenuti nel mio portfolio artistico DarkManga. Qui troverai le mie opere, eventi e informazioni per le commissioni."
 };
 
 const mieiDisegni = [
   {
-    titolo: "Primo Disegno",
-    tecnica: "Inchiostrazione digitale",
-    immagine: "https://via.placeholder.com/600x800"
+    titolo: "Ritratto a Matita Collab",
+    tecnica: "Penna su Carta",
+    immagine: "immagini/immagineprova.jpg",
+    concetto: "Un'interpretazione personale che unisce lo stile manga con elementi iperrealistici e sfumature horror, focalizzata sull'espressività e sul contrasto netto.",
+    dettagliTecnici: "Realizzato interamente a mano libera utilizzando diverse tipologie di penne a sfera (biro) per dosare la pressione e creare sfumature graduali di nero e grigio."
+  }, // <-- LA VIRGOLA MANCANTE ERA QUI
+  {
+    titolo: "Ritratto a Matita Collab 2",
+    tecnica: "Penna su Carta",
+    immagine: "immagini/immagine2.jpg",
+    concetto: "Un'interpretazione personale che unisce lo stile manga con elementi iperrealistici e sfumature horror, focalizzata sull'espressività e sul contrasto netto.",
+    dettagliTecnici: "Realizzato interamente a mano libera utilizzando diverse tipologie di penne a sfera (biro) per dosare la pressione e creare sfumature graduali di nero e grigio."
   }
+];
+  /* 
+  Esempio per aggiungere un secondo lavoro:
+  ,{
+    titolo: "Titolo Secondo Disegno",
+    tecnica: "Acquerello e China",
+    immagine: "immagini/nome-file.jpg",
+    concetto: "Descrizione del concetto artistico...",
+    dettagliTecnici: "Dettagli sui materiali usati..."
+  }
+  */
 ];
