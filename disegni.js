5a5325f7685f81123108a9435cbd96ee371f63b8
SCARMARIANI-creator <debora.mariani@veronatrento.it>
2026-07-29 10:19:14 +0200

    Update disegni.js

--- disegni.js
+++ disegni.js
@@ -1,3 +1,10 @@
+const infoArtista = {
+  nomeLogo: "darkmanga",
+  nome: "Art by Scar",
+  slogan: "Digital Artist",
+  biografia: "Benvenuti nel mio portfolio artistico DarkManga. Qui troverai le mie opere, eventi e informazioni per le commissioni."
+};
+
 const mieiDisegni = [
   {
     titolo: "Ritratto a Matita Collab",
@@ -5,14 +12,15 @@ const mieiDisegni = [
     immagine: "immagini/immagineprova.jpg",
     concetto: "Un'interpretazione personale che unisce lo stile manga con elementi iperrealistici e sfumature horror, focalizzata sull'espressività e sul contrasto netto.",
     dettagliTecnici: "Realizzato interamente a mano libera utilizzando diverse tipologie di penne a sfera (biro) per dosare la pressione e creare sfumature graduali di nero e grigio."
-  }
+  }, // <-- LA VIRGOLA MANCANTE ERA QUI
   {
-    titolo: "Ritratto a Matita Collab",
+    titolo: "Ritratto a Matita Collab 2",
     tecnica: "Penna su Carta",
     immagine: "immagini/immagine2.jpg",
     concetto: "Un'interpretazione personale che unisce lo stile manga con elementi iperrealistici e sfumature horror, focalizzata sull'espressività e sul contrasto netto.",
     dettagliTecnici: "Realizzato interamente a mano libera utilizzando diverse tipologie di penne a sfera (biro) per dosare la pressione e creare sfumature graduali di nero e grigio."
   }
+];
   /* 
   Esempio per aggiungere un secondo lavoro:
   ,{
