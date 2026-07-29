0307091da40ef9682cee63ed3bc7171096dad57a
SCARMARIANI-creator <debora.mariani@veronatrento.it>
2026-07-29 10:43:37 +0200

    Update disegni.js

--- disegni.js
+++ disegni.js
@@ -1,34 +1,8 @@
-const infoArtista = {
-  nomeLogo: "darkmanga",
-  nome: "Art by Scar",
-  slogan: "Digital Artist",
-  biografia: "Benvenuti nel mio portfolio artistico DarkManga. Qui troverai le mie opere, eventi e informazioni per le commissioni."
-};
-
 const mieiDisegni = [
   {
     titolo: "Ritratto a Matita Collab",
     tecnica: "Penna su Carta",
     immagine: "immagini/immagineprova.jpg",
-    concetto: "Un'interpretazione personale che unisce lo stile manga con elementi iperrealistici e sfumature horror, focalizzata sull'espressività e sul contrasto netto.",
-    dettagliTecnici: "Realizzato interamente a mano libera utilizzando diverse tipologie di penne a sfera (biro) per dosare la pressione e creare sfumature graduali di nero e grigio."
-  }, // <-- LA VIRGOLA MANCANTE ERA QUI
-  {
-    titolo: "Ritratto a Matita Collab 2",
-    tecnica: "Penna su Carta",
-    immagine: "immagini/immagine2.jpg",
-    concetto: "Un'interpretazione personale che unisce lo stile manga con elementi iperrealistici e sfumature horror, focalizzata sull'espressività e sul contrasto netto.",
-    dettagliTecnici: "Realizzato interamente a mano libera utilizzando diverse tipologie di penne a sfera (biro) per dosare la pressione e creare sfumature graduali di nero e grigio."
+    descrizione: "interpretazione del mio stile di un personaggio,il mio stile consiste nel rendere lo stile manga iperealistico e horror, ho usato diverse biro per realizzare il disegno."
   }
-];
-  /* 
-  Esempio per aggiungere un secondo lavoro:
-  ,{
-    titolo: "Titolo Secondo Disegno",
-    tecnica: "Acquerello e China",
-    immagine: "immagini/nome-file.jpg",
-    concetto: "Descrizione del concetto artistico...",
-    dettagliTecnici: "Dettagli sui materiali usati..."
-  }
-  */
 ];
