42320226d7b945e8f84e94a9cc1b504b7a248eb6
SCARMARIANI-creator <debora.mariani@veronatrento.it>
2026-07-29 12:06:56 +0200

    Update disegni.js

--- disegni.js
+++ disegni.js
@@ -1,22 +1,14 @@
-var infoArtista = {
-  nomeLogo: "darkmanga",
-  nome: "Art by Scar",
-  slogan: "L'immaginazione è fatta d'inchiostro",
-  biografia: "Benvenuti nel mio portfolio artistico DarkManga. Il mio stile consiste nel rendere lo stile manga iperrealistico e horror, utilizzando principalmente penne biro e tecniche miste.",
-  instagram: "https://instagram.com",
-  email: "tuamail@esempio.com",
-  mastodon: "https://mastodon.social",
-  linktree: "https://linktr.ee"
-};
-
 var mieiDisegni = [
   {
     titolo: "Ritratto a Matita Collab",
     tecnica: "Penna su Carta",
     immagine: "immagini/immagineprova.jpg",
     descrizione: "Interpretazione nel mio stile di un personaggio."
+  },
+  {
+    titolo: "Titolo Nuova Opera",
+    tecnica: "Biro e Inchiostro",
+    immagine: "immagini/nuovo-disegno.jpg",
+    descrizione: "Descrizione del nuovo disegno."
   }
 ];
-
-var mieiEventi = [];
-var mieiProdotti = [];
